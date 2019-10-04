21452a7fc84d080f98459a2cdf57f084467516cc
s-weigand <s.weigand.phy@gmail.com>
2019-10-04 21:49:11 +0200

    added double quote to prevent globbing and word splitting.
    recommandation from codacy.com

--- binder/trigger_binder.sh
+++ binder/trigger_binder.sh
@@ -3,27 +3,25 @@
 # https://github.com/scikit-hep/uproot/blob/master/binder/trigger_binder.sh
 
 function trigger_binder() {
-    if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-        local URL="${1}"
+    local URL="${1}"
 
-        curl -L --connect-timeout 10 --max-time 30 "${URL}"
-        curl_return=$?
+    curl -L --connect-timeout 10 --max-time 30 "${URL}"
+    curl_return=$?
 
-        # Return code 28 is when the --max-time is reached
-        if [ "${curl_return}" -eq 0 ] || [ "${curl_return}" -eq 28 ] ; then
-            if [[ "${curl_return}" -eq 28 ]]; then
-                printf "\nBinder build started.\nCheck back soon.\n"
-            fi
-        else
-            return "${curl_return}"
+    # Return code 28 is when the --max-time is reached
+    if [ "${curl_return}" -eq 0 ] || [ "${curl_return}" -eq 28 ] ; then
+        if [[ "${curl_return}" -eq 28 ]]; then
+            printf "\nBinder build started.\nCheck back soon.\n"
         fi
+    else
+        return "${curl_return}"
     fi
     return 0
 }
 
 function main() {
     # 1: the Binder build API URL to curl
-    trigger_binder $1
+    trigger_binder "$1"
 }
 
 main "$@" || exit 1
