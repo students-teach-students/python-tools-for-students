a7c57eb98766b86867b914a4f2220702175dabf9
s-weigand <s.weigand.phy@gmail.com>
2019-12-02 05:53:01 +0100

    renamed 'refresh_symlinks_windows.sh' to 'refresh_symlinks.sh'
    and made link generation less aggressive

--- docs/refresh_symlinks.sh
+++ docs/refresh_symlinks.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 # Run this in gitbash with administrator rights, if you are on windows
-# or the docs won't build properly.
+# and the docs won't build properly.
 export MSYS=winsymlinks:nativestrict
-ln -srf ../README.md ./readme.md
-ln -srf ../CONTRIBUTING.md ./contributing.md
-ln -srf ../material/cheat_sheets.md ./material/cheat_sheets.md
-ln -srf ../material/tutorials.md ./material/tutorials.md
+ln -srf ../README.md readme.md
+ln -srf ../CONTRIBUTING.md contributing.md
+ln -srf ../material/cheat_sheets.md material/cheat_sheets.md
+ln -srf ../material/tutorials.md material/tutorials.md
--- docs/refresh_symlinks_windows.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# Run this in gitbash with administrator rights, if you are on windows
-# and the docs won't build properly.
-export MSYS=winsymlinks:nativestrict
-rm -f readme.md
-ln -s ../README.md readme.md
-rm -f contributing.md
-ln -s ../CONTRIBUTING.md contributing.md
